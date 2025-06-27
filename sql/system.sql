500e8396fa1179040cea9e74cf743ea3fa72dc98
220-15\dlgksmf0304 <dlgksmf0304@gmail.com>
2025-06-27 15:26:30 +0900

    2025년 6월 27일 백업

--- sql/system.sql
+++ sql/system.sql
@@ -3,5 +3,3 @@ create user boardtest identified by boardtest
 grant resource, connect to boardtest 
 alter user boardtest default tablespace users 
 alter user boardtest temporary tablespace temp 
-
-
